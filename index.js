d6825a138734e13497d1ed1b24c8e869b042c300
Guillermo Alvarez <guillermo@icalialabs.com>
2021-10-30 13:24:02 -0600

    v1 done

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-console.log('runs')
-
 const darkAstronauts = [1, 2, 3];
 const lightAstronauts = [4, 5];
+
+const min = 1;
+const max = 5;
+const astronaut = Math.floor(Math.random() * (max - min + 1) + min)
+
+if (lightAstronauts.includes(astronaut)) {
+  document.querySelectorAll(".dark").forEach(node => {
+    node.classList.remove("dark")
+  })
+}
+
+const player = document.querySelector("lottie-player");
+player.load(`./assets/${astronaut}.json`);
